115cd7b09b4a6907a82c9ed9817d8a469f266fef
Kai Chew <kafechew@gmail.com>
2025-06-18 15:50:41 +0800

    no editpost button. more posts per page.

--- src/config.ts
+++ src/config.ts
@@ -6,13 +6,13 @@ export const SITE = {
   title: "kheAI",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 5,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Suggest Changes",
     url: "https://github.com/kafechew/astro-paper/edit/main/",
   },
